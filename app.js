78be80518ab025b923195f9d533fa9903a7a8bd0
MdSajjadHossin <iamsajjadhossin@gmail.com>
2021-09-02 05:03:36 +0600

    third-push

--- app.js
+++ app.js
@@ -8,44 +8,29 @@ const searchBook = () =>{
 
     // search data
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
-    console.log(url);
+    // console.log(url);
     fetch(url)
     .then(res => res.json())
     .then(data => displaySearchResult(data))
 
 }
-//load image
-const displayImage = loadImg =>{
-        const url = `https://covers.openlibrary.org/b/id/${loadImg}-M.jpg`;
-        console.log(url);
-        fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data))
-}
-//load Author Details
-const displayAuthor = authhorKey =>{
-        const url = `https://covers.openlibrary.org/b/id/${authhorKey}-L.jpg`;
-        console.log(url);
-        fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data))
-}
-
+// Displaying Result
 const displaySearchResult = books => {
     const searchResult = document.getElementById('search-result');
     // books.forEach(book => {
         for (const book in books.docs)
         {
             books.docs.forEach(eachBook =>{
+                imgUrl = `https://covers.openlibrary.org/b/id/${eachBook.cover_i}-M.jpg`;
                 // console.log(book);
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
             <div class="card h-100">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="${imgUrl}" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title">Book Title: ${eachBook.title}</h5>
-            <h5 class="card-title">Author Name: ${eachBook.name}</h5>
+            <h5 class="card-title">Author Name: ${eachBook.author_name}</h5>
             <h5 class="card-title">Publisher: ${eachBook.publisher}</h5>
             <h5 class="card-title">Publish Year: ${eachBook.first_publish_year}</h5>
             </div>
