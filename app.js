c6c5c6af5b1f1065fbb9f84e7a1e918dd783adfa
MdSajjadHossin <iamsajjadhossin@gmail.com>
2021-09-02 09:28:21 +0600

    final-commit

--- app.js
+++ app.js
@@ -14,33 +14,47 @@ const searchBook = () =>{
     .then(data => displaySearchResult(data))
 
 }
+// searchBtn.addEventListener("click", function () {
+//     console.log(spinner);
+//     const searchText = searchInput.value;
+//     if (searchText === "") {
+//       errorDiv.innerText = "Search field cannot be empty.";
+//       return;
+//     }
+// })
+
 // Displaying Result
 const displaySearchResult = books => {
     const searchResult = document.getElementById('search-result');
-    // books.forEach(book => {
+    searchResult.textContent = '';
+    const totalResultFound =  document.getElementById('result-found');
+    totalResultFound.innerHTML = `
+    <p class= "fs-4 mx-auto">Total Result found: ${books.numFound}</p>
+     `
         for (const book in books.docs)
-        {
+        { 
             books.docs.forEach(eachBook =>{
+                // const totalResultFound =  document.getElementById('result-found');
+                // totalResultFound.innerHTML = `
+                // <p class= "fs-4 mx-auto">Total Result found: ${book.numFound}</p>
+                // `
                 imgUrl = `https://covers.openlibrary.org/b/id/${eachBook.cover_i}-M.jpg`;
-                // console.log(book);
-            const div = document.createElement('div');
-            div.classList.add('col');
-            div.innerHTML = `
-            <div class="card h-100">
-            <img src="${imgUrl}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">Book Title: ${eachBook.title}</h5>
-            <h5 class="card-title">Author Name: ${eachBook.author_name}</h5>
-            <h5 class="card-title">Publisher: ${eachBook.publisher}</h5>
-            <h5 class="card-title">Publish Year: ${eachBook.first_publish_year}</h5>
-            </div>
-            </div>
-        `;
-        searchResult.appendChild(div);
-        });
-            
+                // console.log(eachBook);
+                const div = document.createElement('div');
+                div.classList.add('col');
+                div.innerHTML = `
+                <div class="card h-100">
+                <img src="${imgUrl}" class="card-img-top card-img" alt="...">
+                <div class="card-body">
+                <h5 class="card-title"><span class="fs-5 my-auto fw-bold">Book Title:</span> <span class="fs-6">${eachBook.title}</span></h5>
+                <h5 class="card-title"><span class="fs-6 my-auto fw-bold">Author Name:</span> <span class="fs-6">${eachBook.author_name}</span></h5>
+                <h5 class="card-title"><span class="fs-6 my-auto fw-bold">Publisher:</span> <span class="fs-6">${eachBook.publisher}</span></h5>
+                <h5 class="card-title"><span class="fs-6 my-auto fw-bold">Publish Year:</span> <span class="fs-6">${eachBook.first_publish_year}</span></h5>
+                </div>
+                </div>
+                `;
+                searchResult.appendChild(div);  
+        });  
+          
     }
-        
-    
- 
 }
